698f9b205903528b3161788abd7dea7c6ccd91fa
Julnowak <julnowak@student.agh.edu.pl>
2025-05-04 23:49:10 +0200

    Database fixed - DO NOT TOUCH WITHOUT WRITTEN PERMISSION

--- backend/mainApp/urls.py
+++ backend/mainApp/urls.py
@@ -2,8 +2,6 @@ from . import views
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import NotificationsAPI, NewsAPI
-
 urlpatterns = [
     path('register/', views.UserRegister.as_view(), name='register'),
     path('login/', views.UserLogin.as_view(), name='login'),
@@ -14,9 +12,9 @@ urlpatterns = [
     path('buildings/', views.BuildingsAllData.as_view(), name='buildings'),
     path('building/<str:building_name>', views.BuildingAPI.as_view(), name='building'),
     path('notifications/', views.NotificationsAPI.as_view(), name='notifications'),
-    path('notifications/<int:pk>/', NotificationsAPI.as_view(), name='notification-detail'),
+    path('notifications/<int:pk>/', views.NotificationsAPI.as_view(), name='notification-detail'),
     path('news/', views.NewsAPI.as_view(), name='news'),
-    path('news/<int:pk>/', NewsAPI.as_view(), name='news-detail'),
+    path('news/<int:pk>/', views.NewsAPI.as_view(), name='news-detail'),
     path('sources/', views.SourceAPI.as_view(), name='sources'),
     path('fields/', views.FieldsAPI.as_view(), name='fields'),
     path('maturasubjects/', views.MaturaSubjectsAPI.as_view(), name='maturasubjects'),
