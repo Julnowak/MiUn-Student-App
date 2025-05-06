335a50ac14fb6709ec999c63f13650716c9801d7
Julnowak <julnowak@student.agh.edu.pl>
2025-05-06 14:28:38 +0200

    mistakes fixed - sqlite3 recovery

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,16 +96,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
 
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://miundb:5Q8H7TI3zDnJrYzy941eVOsWcsN2jyQu@dpg-d0cv3t8dl3ps73ek03v0-a.oregon-postgres.render.com/miundb',
-        conn_max_age=600
-    ),
+    # 'default': dj_database_url.config(
+    #     # Replace this value with your local database's connection string.
+    #     default='postgresql://miundb:5Q8H7TI3zDnJrYzy941eVOsWcsN2jyQu@dpg-d0cv3t8dl3ps73ek03v0-a.oregon-postgres.render.com/miundb',
+    #     conn_max_age=600
+    # ),
 
-    'sqlite': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'sqlite': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
